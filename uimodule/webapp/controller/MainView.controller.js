906a7f57dbe8054417ff23bbc5216e763ae4b484
Frank Hufnagel <frank.hufnagel@sap.com>
2021-10-01 10:14:28 +0200

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,5 @@
+sap.ui.define(["com/sap/demoui5/controller/BaseController"], function (Controller) {
+    "use strict";
+
+    return Controller.extend("com.sap.demoui5.controller.MainView", {});
+});
